290fc2f345b19eadcc098b52f5a8cb66df58caf5
Carina Franing Rutledal <carina.rutledal@visma.com>
2023-01-09 16:39:43 +0100

    fjernet fasit og eksempel på switch expression

--- src/main/java/com/twoday/javafagkveld/Switch/AustralianAnimals.java
+++ src/main/java/com/twoday/javafagkveld/Switch/AustralianAnimals.java
@@ -4,25 +4,7 @@ public class AustralianAnimals {
 
     enum AnimalsInAustralia{SPIDER, SNAKE, KOALA, KANGAROO};
 
-    String animalDescription(AnimalsInAustralia animals) {
-        var description = switch(animals) {
-            case SNAKE, SPIDER ->  "scary looking";
-            case KOALA -> {yield "Soooo cute";}
-            case KANGAROO -> "very jumpy";
-        };
-        return description;
-    }
-
     enum AnimalFood{INSECTS, RATS, LEAVES};
-    AnimalFood animalFood(AnimalsInAustralia animal) {
-        var food = switch(animal) {
-            case KOALA, KANGAROO -> AnimalFood.LEAVES;
-            case SPIDER -> AnimalFood.INSECTS;
-            case SNAKE -> AnimalFood.RATS;
-        };
-        return food;
-    }
-
     AnimalFood animalFoodGammel(AnimalsInAustralia animal) {
         AnimalFood food;
         switch(animal) {

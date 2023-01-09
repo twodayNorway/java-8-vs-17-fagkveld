c3693409781c553f9543b1ce35c4bdb1edd3b79b
Carina Franing Rutledal <carina.rutledal@visma.com>
2023-01-09 17:05:31 +0100

    Lagt til klasse 'Oppgaver', med textBlock funkjson

---
+++ src/main/java/com/twoday/javafagkveld/AvsluttendeOppgaver/Oppgaver.java
@@ -0,0 +1,19 @@
+package com.twoday.javafagkveld.AvsluttendeOppgaver;
+
+public class Oppgaver {
+
+    public static String animalTextBlock() {
+        String animals = "Av mange dyr i Australia kan vi nevne noen: \nKoala \nKenguru \nslanger \nklovnefisk, også kalt \"nemo\"fisk";
+        //TODO: skriv om animals til å være en text block
+        return animals;
+    }
+
+    public static String htmlTextBlock() {
+        String htmlKode = "<button>\n";
+    }
+
+
+
+
+
+}
--- src/main/java/com/twoday/javafagkveld/Main.java
+++ src/main/java/com/twoday/javafagkveld/Main.java
@@ -1,21 +1,25 @@
 package com.twoday.javafagkveld;
 
+import com.twoday.javafagkveld.AvsluttendeOppgaver.Oppgaver;
 import com.twoday.javafagkveld.Records.Forest;
 import com.twoday.javafagkveld.Switch.AustralianAnimals;
 
 public class Main {
     public static void main(String[] args) {
-        // TODO: For hver oppgave, kommenter ut den
-        // TODO: forrige og fjern kommentar fra neste
-        // TODO: og kjør.
-        // Kanskje lage tester istedenfor?
+        // TODO: For hver oppgave, kommenter ut den forrige og kjør.
 
         // Records
         // Skal printe: "Koala Coco er 4 år gammel, og har spist 20 blader i dag."
         Forest forest = new Forest();
         System.out.println(forest.findCocoTheKoala());
 
+        // Switch expressions
+        // Skal printe "LEAVES"
         AustralianAnimals aa = new AustralianAnimals();
-        aa.animalFoodGammel(AustralianAnimals.AnimalsInAustralia.KANGAROO);
+        System.out.println(aa.animalFoodGammel(AustralianAnimals.AnimalsInAustralia.KANGAROO));
+
+        // Text Blocks
+        // Skal ramse opp Koala, Kenguru, Slanger og Klovnefisk under hverandre
+        System.out.println(Oppgaver.animalTextBlock());
     }
 }
